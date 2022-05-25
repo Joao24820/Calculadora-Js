cc201b18d7079de93748f5162e4d688312c4972d
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-05-25 20:08:47 -0300

    Create Calculadora.js

---
+++ Calculadora.js
@@ -0,0 +1,16 @@
+var n1 = prompt('Digite um numero: ')
+
+function cal(){
+    document.write(n1 + ' *  1 ','=' + n1 * 1)
+    document.write(n1 + ' *  1 ','=' + n1 * 2)   
+    document.write(n1 + ' *  1 ','=' + n1 * 3)   
+    document.write(n1 + ' *  1 ','=' + n1 * 4)   
+    document.write(n1 + ' *  1 ','=' + n1 * 5)   
+    document.write(n1 + ' *  1 ','=' + n1 * 6)   
+    document.write(n1 + ' *  1 ','=' + n1 * 7)
+    document.write(n1 + ' *  1 ','=' + n1 * 8)
+    document.write(n1 + ' *  1 ','=' + n1 * 9)
+    document.write(n1 + ' *  1 ','=' + n1 * 10)       
+}
+
+document.write(cal)
